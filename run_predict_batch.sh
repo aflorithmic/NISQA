4882e735aa7544abf08374de85418db3073a6cfb
evelyndjwilliams <evelyn@aflorithmic.ai>
2022-03-22 11:50:29 +0000

    added command line flags to handle quality/naturalness modes

--- run_predict_batch.sh
+++ run_predict_batch.sh
@@ -8,12 +8,37 @@ else
     . /opt/conda/etc/profile.d/conda.sh && conda deactivate && conda activate nisqa
 fi
 
-for speaker_dir in to_evaluate/input/*/; do
-    speaker=$(basename $speaker_dir)
-    echo $speaker $speaker_dir
-    mkdir -p ./to_evaluate/results/quality/"$speaker"
-    mkdir -p ./to_evaluate/results/naturalness/"$speaker"
-    python ./run_predict.py --mode predict_dir --pretrained_model weights/nisqa.tar --data_dir "${speaker_dir}" --num_workers 0 --bs 10 --output_dir to_evaluate/results/quality/"${speaker}"
-    python ./run_predict.py --mode predict_dir --pretrained_model weights/nisqa_tts.tar --data_dir "${speaker_dir}" --num_workers 0 --bs 10 --output_dir to_evaluate/results/naturalness/"${speaker}"
-    
-done
+n_flag=''
+q_flag=''
+
+print_usage() {
+  printf "Usage: ..."
+}
+
+while getopts 'nq' flag; do
+  case "${flag}" in
+    n) n_flag='true' ;;
+    q) q_flag='true' ;;
+    *) print_usage
+       exit 1 ;;
+    esac
+done 
+
+if $n_flag; then
+    for speaker_dir in to_evaluate/input/*/; do
+        speaker=$(basename $speaker_dir)
+        echo $speaker $speaker_dir
+        mkdir -p ./to_evaluate/results/naturalness/"$speaker"
+        python ./run_predict.py --mode predict_dir --pretrained_model weights/nisqa_tts.tar --data_dir "${speaker_dir}" --num_workers 0 --bs 10 --output_dir to_evaluate/results/naturalness/"${speaker}"
+        
+    done
+fi
+
+if $n_flag; then
+    for speaker_dir in to_evaluate/input/*/; do
+        speaker=$(basename $speaker_dir)
+        echo $speaker $speaker_dir
+        mkdir -p ./to_evaluate/results/quality/"$speaker"
+        python ./run_predict.py --mode predict_dir --pretrained_model weights/nisqa.tar --data_dir "${speaker_dir}" --num_workers 0 --bs 10 --output_dir to_evaluate/results/quality/"${speaker}"
+    done
+fi
